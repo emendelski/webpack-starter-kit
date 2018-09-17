0f65e6fb808ba2ede7762b076db7f9da41213581
Kamil Mendelski <kamil@frontlabs.pl>
2018-09-17 07:49:42 +0200

    Removed lodash from project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import _ from 'lodash';
-
 import './scss/main.scss';
 
 function component() {
   const element = document.createElement('div');
-  element.innerHTML = _.join(['webpack', 'scss', 'autoprefixer', 'babel', 'eslint', 'stylelint', 'ftp-deploy'], ' &bull; ');
+  element.innerHTML = ['webpack', 'scss', 'autoprefixer', 'babel', 'eslint', 'stylelint', 'ftp-deploy'].join(' &bull; ');
 
   return element;
 }
