bc1f3f4306ed1eebfc6f6ca33a55d6eb3965e5bd
Kamil Mendelski <kamil@frontlabs.pl>
2018-09-17 07:30:54 +0200

    Home page redesign & simplified

--- src/main.js
+++ src/main.js
@@ -4,12 +4,9 @@ import './scss/main.scss';
 
 function component() {
   const element = document.createElement('div');
-  const p = [1, 2, 3].map(n => n + 1);
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack', 'my', 'dear', 'friend:', p], ' ');
+  element.innerHTML = _.join(['webpack', 'scss', 'autoprefixer', 'babel', 'eslint', 'stylelint', 'ftp-deploy'], ' &bull; ');
 
   return element;
 }
 
-document.body.appendChild(component());
+document.querySelector('.content').appendChild(component());
