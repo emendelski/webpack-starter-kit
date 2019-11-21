e7956744f4382c38911f0abd4643ec9c60623085
Piotr Misiuk <pmisiuk@gmail.com>
2019-11-21 17:51:12 +0100

    Using Bootstrap for demo

--- docs/src/docs.js
+++ docs/src/docs.js
@@ -4,9 +4,8 @@ import 'prismjs';
 import 'prismjs/plugins/unescaped-markup/prism-unescaped-markup';
 import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
 
-const main = document.querySelector('.js-main');
-const nav = document.querySelector('.js-nav');
-const sections = main.querySelectorAll('.section');
+const nav = document.querySelector('.fldocs-js-nav');
+const sections = document.querySelectorAll('.fldocs-section');
 
 const createLink = (type, name, target) => {
   const li = document.createElement('li');
@@ -14,18 +13,18 @@ const createLink = (type, name, target) => {
 
   link.innerText = name;
   link.setAttribute('href', `#${target}`);
-  link.classList.add(type, 'nav__link');
+  link.classList.add(`fldocs-nav__link--${type}`, 'fldocs-nav__link');
 
-  li.classList.add('nav__item');
+  li.classList.add('fldocs-nav__item');
 
   li.appendChild(link);
   nav.appendChild(li);
 };
 
 sections.forEach((section) => {
-  const h2 = section.querySelector('h2');
-  const h3s = section.querySelectorAll('h3');
+  const h2 = section.querySelector('.fldocs-h2');
+  const h3s = section.querySelectorAll('.fldocs-h3');
 
-  createLink('h2', h2.innerText, h2.id);
-  h3s.forEach(heading => createLink('h3', heading.innerText, heading.id));
+  if (h2) createLink('h2', h2.innerText, h2.id);
+  if (h3s.length) h3s.forEach(heading => createLink('h3', heading.innerText, heading.id));
 });
