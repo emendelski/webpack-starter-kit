5c7361312d2e56194a88a2226be5d248e00b3807
Kamil Mendelski <kamil@frontlabs.pl>
2019-11-22 10:00:14 +0100

    Used normalize-sass & basic layout modifications

--- docs/src/docs.js
+++ docs/src/docs.js
@@ -4,8 +4,8 @@ import 'prismjs';
 import 'prismjs/plugins/unescaped-markup/prism-unescaped-markup';
 import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
 
-const nav = document.querySelector('.fldocs-js-nav');
-const sections = document.querySelectorAll('.fldocs-section');
+const nav = document.querySelector('.-docs-js-nav');
+const sections = document.querySelectorAll('.-docs-section');
 
 const createLink = (type, name, target) => {
   const li = document.createElement('li');
@@ -13,17 +13,17 @@ const createLink = (type, name, target) => {
 
   link.innerText = name;
   link.setAttribute('href', `#${target}`);
-  link.classList.add(`fldocs-nav__link--${type}`, 'fldocs-nav__link');
+  link.classList.add(`-docs-nav__link--${type}`, '-docs-nav__link');
 
-  li.classList.add('fldocs-nav__item');
+  li.classList.add('-docs-nav__item');
 
   li.appendChild(link);
   nav.appendChild(li);
 };
 
 sections.forEach((section) => {
-  const h2 = section.querySelector('.fldocs-h2');
-  const h3s = section.querySelectorAll('.fldocs-h3');
+  const h2 = section.querySelector('.-docs-h2');
+  const h3s = section.querySelectorAll('.-docs-h3');
 
   if (h2) createLink('h2', h2.innerText, h2.id);
   if (h3s.length) h3s.forEach(heading => createLink('h3', heading.innerText, heading.id));
