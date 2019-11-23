0b97ce6da5edcf5a373f65de58e67337bfe62240
Kamil Mendelski <kamil@frontlabs.pl>
2019-11-23 14:31:04 +0100

    Included babel polyfills into code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './scss/main.scss';
 
+// Babel for previous JS support
+import 'core-js';
+import 'regenerator-runtime/runtime';
+
+// Local components
 function component() {
   const element = document.createElement('div');
   element.innerHTML = ['webpack', 'scss', 'autoprefixer', 'babel', 'eslint', 'stylelint', 'ftp-deploy'].join(' &bull; ');
